421e93c188aa821cc6625b146c89f5dfdf2148c9
d1maal <dimaal99@mail.ru>
2025-12-27 10:14:38 +0000

    task_02

--- task_02/src/main.cpp
+++ task_02/src/main.cpp
@@ -1,3 +1,27 @@
-#include <iostream>
+#include "graph.h"
 
-int main() { return 0; }
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ios_base;
+using std::pair;
+using std::vector;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+  vector<pair<int, int>> edges(m);
+  for (int i = 0; i < m; ++i) {
+    cin >> edges[i].first >> edges[i].second;
+  }
+
+  cout << MinEdgesToStronglyConnected(n, edges) << endl;
+
+  return 0;
+}
